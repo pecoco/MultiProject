5be8529f6aa49f93923e6514f404433721787ad7
VittualMask <virtual-mask@abox3.so-net.ne.jp>
2016-07-19 07:53:03 +0900

    TimeLinetoXML　First　Commit

---
+++ TimeLinetoDB/TimeLine2XML/TimeLineXML.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace TimeLineXML
+{
+    class TimeLineXML
+    {
+
+        List<Alertall> alertalls;
+        List<TimeLine> timeLines;
+    }
+
+
+
+    public class Alertall{
+        bool commentOut;
+        int id;
+        string title;
+        int before;
+        string acter;
+        string messege;
+    }
+
+    public class TimeLine
+    {
+        bool commentOut;
+        int id;
+        int sec;
+        string title;
+        string syncString;
+        int duration;
+        int window; 
+    }
+
+}

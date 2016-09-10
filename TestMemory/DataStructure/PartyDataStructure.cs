30fd2f69b33b3a174b32361e078e15019c66db4f
VittualMask <virtual-mask@abox3.so-net.ne.jp>
2016-09-11 01:42:24 +0900

    FFIVXTestMemory

---
+++ TestMemory/DataStructure/PartyDataStructure.cs
@@ -0,0 +1,25 @@
+﻿using Commonm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMemory.DataStructure
+{
+    public class PartyDataStructure
+    {
+        public string Name { get; set; }
+        public uint ID { get; set; }
+        public double X { get; set; }
+        public double Z { get; set; }
+        public double Y { get; set; }
+        public Enumes.Job Job { get; set; }
+        public byte Level { get; set; }
+        public int HPCurrent { get; set; }
+        public int HPMax { get; set; }
+        public int MPCurrent { get; set; }
+        public int MPMax { get; set; }
+        //List<StatusEntry> StatusEntries { get; set; }
+    }
+}
---
+++ TestMemory/DataStructure/PartyInfo.cs
@@ -0,0 +1,41 @@
+﻿using Commonm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMemory.Helper;
+
+namespace TestMemory.DataStructure
+{
+    public class PartyInfo
+    {
+        public static PartyDataStructure d;
+        public static void ResolvePartyFromBytes(byte[] source)
+        {
+            var entry = new PartyDataStructure();
+
+            try
+            {                
+                entry.X = BitConverter.ToSingle(source, 0x0);
+                entry.Z = BitConverter.ToSingle(source, 0x4);
+                entry.Y = BitConverter.ToSingle(source, 0x8);
+                //entry.Coordinate = new Coordinate(entry.X, entry.Z, entry.Z);
+                entry.ID = BitConverter.ToUInt32(source, 0x10);
+                entry.Name = MemoryLib.GetStringFromBytes(source, 0x20);
+                entry.Job = (Enumes.Job)source[0x61];
+                entry.Level = source[0x63];
+                entry.HPCurrent = BitConverter.ToInt32(source, 0x68);
+                entry.HPMax = BitConverter.ToInt32(source, 0x6C);
+                entry.MPCurrent = BitConverter.ToInt16(source, 0x70);
+                entry.MPMax = BitConverter.ToInt16(source, 0x72);
+            }
+            catch(Exception e)
+            {
+
+            }
+            d = entry;
+        }
+        public static int PartyCount;
+    }
+}
